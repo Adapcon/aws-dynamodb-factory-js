05b44afa2fe3a4e4c8bf8ac53fe3b3bee329df66
kelvinoenning <kelvinstang@hotmail.com>
2018-05-08 10:34:44 -0300

    add custom offline port

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
 const AWS = require('aws-sdk')
 
-const DEFAULT_PRODUCTION_OPTIONS = {
-    region: 'sa-east-1'
-}
-const DEFAULT_LOCAL_OPTIONS = {
-    region: "localhost",
-    endpoint: "http://localhost:8000"
-}
-
 class DynamodbFactory {
-    static raw(options) {
+    static raw({options} = {}) {
         return new AWS.DynamoDB(this._getOptions(options))
     }
     
-    static doc(options) {
+    static doc({options} = {}) {
         return new AWS.DynamoDB.DocumentClient(this._getOptions(options))
     }
 
-    static _getOptions(options) {
+    static _getOptions({options, offlinePort} = {}) {
         if (options) return options;
-        else if (process.env.IS_OFFLINE) return DEFAULT_LOCAL_OPTIONS;
-        else return DEFAULT_PRODUCTION_OPTIONS;
+        else if (process.env.IS_OFFLINE) return { region: "localhost", endpoint: `http://localhost:${offlinePort || 8000}` }
+        else return { region: 'sa-east-1' };
     }
 }
 
--- test/index.spec.js
+++ test/index.spec.js
@@ -33,10 +33,20 @@ describe('UTIL - DynamodbFactory', () => {
                 expect(options.region).to.equal('localhost')
                 expect(options.endpoint).to.equal('http://localhost:8000')
             })
+            it('should return local options with custom port', () => {
+                process.env.IS_OFFLINE = true;
+                let options = DynamodbFactory._getOptions({
+                    offlinePort: 3001
+                });
+                expect(options.region).to.equal('localhost')
+                expect(options.endpoint).to.equal('http://localhost:3001')
+            })
             it('should return custom options', () => {
                 let options = DynamodbFactory._getOptions({
-                    region: 'customRegion',
-                    endpoint: 'customEndPoint'
+                    options: {
+                        region: 'customRegion',
+                        endpoint: 'customEndPoint'
+                    }
                 });
                 expect(options.region).to.equal(options.region)
                 expect(options.endpoint).to.equal(options.endpoint)
@@ -56,33 +66,4 @@ describe('UTIL - DynamodbFactory', () => {
             })
         });
     });
-
-    /* describe('static functions', () => {
-      it('create', () => {
-        let dynamodbFactoryInstance = new DynamodbFactory();
-  
-        expect(DynamodbFactory.create).to.be.an('function')
-        expect(dynamodbFactoryInstance.create).to.be.an('undefined')
-      });
-    });
-  
-    describe('instance', () => {
-      it('should instance production database', () => {
-        let db = DynamodbFactory.create()
-        expect(db.region).to.equal(PRODUCTION_REGION)
-      });
-      it('should instance local database', () => {
-        let db = DynamodbFactory.create({
-          offline: true
-        })
-        expect(db.region).to.equal(LOCAL_REGION)
-      });
-      it('should instance production database with custom region', () => {
-        let customRegion = 'us-east-1';
-        let db = DynamodbFactory.create({
-          region: customRegion
-        })
-        expect(db.region).to.equal(customRegion)
-      })
-    }); */
 });
