dfd442733f5d2059acee1654ea52d07a09cac4d1
Gabriel de Oliveira <gabrielhk3@gmail.com>
2022-03-31 13:57:13 -0300

    chore: add type declarations

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+import {DynamoDB} from "aws-sdk";
+
+export interface FactoryOptions<T> {
+    options?: T
+    offlinePort?: number
+}
+
+declare class DynamodbFactory {
+    static raw(opts?: FactoryOptions<DynamoDB.Types.ClientConfiguration>): DynamoDB
+
+    static doc(opts?: FactoryOptions<DynamoDB.Types.ClientConfiguration & DynamoDB.DocumentClient.DocumentClientOptions>): DynamoDB.DocumentClient
+
+    static _getOptions<T extends DynamoDB.Types.ClientConfiguration>(opts: FactoryOptions<T>): T
+}
+
+export default DynamodbFactory
